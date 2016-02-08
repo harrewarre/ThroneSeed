35178417739c022d30edb7b43e34664b8eded271
Sander Harrewijnen <sharrewijnen@live.com>
2016-02-08 22:07:56 +0100

    UI done, main logic done. Still needs notifications and error handling.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using ThroneSeed.ViewModels;
 
 namespace ThroneSeed
@@ -21,10 +8,24 @@ namespace ThroneSeed
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly SeederViewModel ViewModel = new SeederViewModel();
+
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new SeederViewModel();
+            Seed.Focus();
+
+            DataContext = ViewModel;
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.SetSeed(Seed.Text);
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            ViewModel.ClearSeed();
         }
     }
 }
--- ViewModels/SeederViewModel.cs
+++ ViewModels/SeederViewModel.cs
@@ -1,19 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace ThroneSeed.ViewModels
 {
     public class SeederViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
         private string SeedValueBackingField;
+        private string SeedFilePath;
 
         public SeederViewModel()
         {
+            SeedFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "nuclearthrone", "seed.txt");
+            SeedValue = LoadSeed();
+        }
+
+        public void SetSeed(string newSeed)
+        {
+            using (var writer = new StreamWriter(SeedFilePath, false))
+            {
+                writer.WriteLine(newSeed);
+            }
+        }
+
+        public void ClearSeed()
+        {
+            if(File.Exists(SeedFilePath))
+            {
+                File.Delete(SeedFilePath);
+            }
+
+            SeedValue = string.Empty;
+        }
+
+        private string LoadSeed()
+        {
+            if(!File.Exists(SeedFilePath))
+            {
+                return string.Empty;
+            }
+
+            using (var reader = new StreamReader(SeedFilePath))
+            {
+                return reader.ReadToEnd().Trim();
+            }
+        }
+
+        public bool IsValidSeed
+        {
+            get
+            {
+                if(string.IsNullOrWhiteSpace(SeedValueBackingField) || SeedValueBackingField.Length > 10)
+                {
+                    return false;
+                }
+
+                long result = 0;
+                return long.TryParse(SeedValueBackingField, out result);
+            }
         }
 
         public string SeedValue
@@ -32,20 +83,6 @@ namespace ThroneSeed.ViewModels
             if (PropertyChanged != null)
             {
                 PropertyChanged(this, new PropertyChangedEventArgs(prop));
-            }
-        }
-
-        public bool IsValidSeed
-        {
-            get
-            {
-                if(string.IsNullOrWhiteSpace(SeedValueBackingField) || SeedValueBackingField.Length > 10)
-                {
-                    return false;
-                }
-
-                long result = 0;
-                return long.TryParse(SeedValueBackingField, out result);
             }
         }
     }
