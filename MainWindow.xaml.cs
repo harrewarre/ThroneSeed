d3e30a46e6aee6b80ecb4f6fdf4c244d38cb5190
Sander Harrewijnen <sharrewijnen@live.com>
2016-02-08 21:22:53 +0100

    UI working

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ThroneSeed.ViewModels;
 
 namespace ThroneSeed
 {
@@ -23,6 +24,7 @@ namespace ThroneSeed
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new SeederViewModel();
         }
     }
 }
---
+++ ViewModels/SeederViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThroneSeed.ViewModels
+{
+    public class SeederViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string SeedValueBackingField;
+
+        public SeederViewModel()
+        {
+        }
+
+        public string SeedValue
+        {
+            get { return SeedValueBackingField; }
+            set
+            {
+                SeedValueBackingField = value;
+                NotifyPropertyChanged("SeedValue");
+                NotifyPropertyChanged("IsValidSeed");
+            }
+        }
+
+        private void NotifyPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+
+        public bool IsValidSeed
+        {
+            get
+            {
+                if(string.IsNullOrWhiteSpace(SeedValueBackingField) || SeedValueBackingField.Length > 10)
+                {
+                    return false;
+                }
+
+                long result = 0;
+                return long.TryParse(SeedValueBackingField, out result);
+            }
+        }
+    }
+}
